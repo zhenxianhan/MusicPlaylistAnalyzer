5c19892f52c2a131df7f120636f85dd6fb50d48c
韩镇先 <hanzhenxian@hanzhenxiandeMacBook-Pro.local>
2019-05-09 22:41:34 -0500

    complete

---
+++ musicstatsloader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace musicanalyzer
+{
+    public static class musicstatsloader
+    {
+       
+            private static int NumItemsInRow = 8;
+
+        public static List<MusicStats> Load(string txtFilePath)
+        {
+            List<MusicStats> musicStatsList = new List<MusicStats>();
+
+            try
+            {
+                using (var reader = new StreamReader(txtFilePath))
+                {
+                   
+                     int lineNumber = 0;
+                    while (!reader.EndOfStream)
+                    {
+                       
+                         var line = reader.ReadLine();
+                        lineNumber++;
+
+                        if (lineNumber == 1) continue;
+
+                        var values = line.Split('\t');
+
+                        if (values.Length != NumItemsInRow)
+                        {
+                            throw new Exception($"Row {lineNumber} contains {values.Length} values. It should contain {NumItemsInRow}.");
+                        }
+                        try
+                        {
+                            string name = values[0];
+                            string artist = values[1];
+                            string album = values[2];
+                            string genre = values[3];
+                            int size = Int32.Parse(values[4]);
+                            double time = Double.Parse(values[5]);
+                            int year = Int32.Parse(values[6]);
+                            int plays = Int32.Parse(values[7]);
+
+                            MusicStats musicStats = new MusicStats(name, artist, album, genre, size, time, year, plays);
+                            musicStatsList.Add(musicStats);
+                        }
+                        catch (FormatException e)
+                        {
+                            throw new Exception($"Row {lineNumber} contains invalid data. ({e.Message})");
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Unable to open {txtFilePath} ({e.Message}).");
+            }
+
+            return musicStatsList;
+        }
+    }
+}
